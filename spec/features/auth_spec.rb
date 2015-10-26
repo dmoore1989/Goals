d20de08787cde1ff3170be6ced96c3c399534555
dmoore1989 <douglas.moore89@gmail.com>
2015-10-26 10:46:45 -0400

    Add user model

---
+++ spec/features/auth_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "the signup process" do
+
+  it "has a new user page"
+    visit('/users/new')
+    expect(page).to have_content('New User')
+
+  feature "signing up a user" do
+
+    it "shows username on the homepage after signup"
+
+  end
+
+end
+
+feature "logging in" do
+
+  it "shows username on the homepage after login"
+
+end
+
+feature "logging out" do
+
+  it "begins with logged out state"
+
+  it "doesn't show username on the homepage after logout"
+
+end
